b7dd08c61f06db3718069556ee45887fb37b5faf
akrck02 <aketza.vazquez@gmail.com>
2025-07-17 18:53:22 +0200

    wip

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	myInput := os.Getenv("INPUT_MYINPUT")
+	output := fmt.Sprintf("Hello %s", myInput)
+	fmt.Println(fmt.Sprintf(`::set-output name=myOutput::%s`, output))
+}
