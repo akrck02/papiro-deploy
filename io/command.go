33327878b5c0e9799cd13f60540f7ff83e815152
akrck02 <aketza.vazquez@gmail.com>
2025-07-17 20:29:26 +0200

    wget does not exist in every docker installation lmao

--- io/command.go
+++ io/command.go
@@ -1,6 +1,8 @@
 package io
 
 import (
+	"io"
+	"net/http"
 	"os"
 	"os/exec"
 	"strings"
@@ -22,8 +24,22 @@ func executeCommand(command string, args ...string) error {
 	return nil
 }
 
-func Wget(url string) error {
-	return executeCommand("wget", url)
+func Wget(url string, path string) error {
+
+	out, error := os.Create(path)
+	if nil != error {
+		return error
+	}
+	defer out.Close()
+
+	resp, error := http.Get(url)
+	if nil != error {
+		return error
+	}
+
+	defer resp.Body.Close()
+	_, error = io.Copy(out, resp.Body)
+	return error
 }
 
 func Move(path string, destination string) error {
