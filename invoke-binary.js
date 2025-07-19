62f1fcfb840b5b6f25a344814d44741befea6e11
akrck02 <aketza.vazquez@gmail.com>
2025-07-19 16:12:01 +0200

    Update invoke-binary.js

--- invoke-binary.js
+++ invoke-binary.js
@@ -1,20 +1,10 @@
-import { spawn } from "node:child_process";
+const { spawnSync } = require("child_process");
 
 function chooseBinary() {
 	return `main-linux-amd64`;
 }
 
 const binary = chooseBinary();
-const mainScript = `./${binary}`;
-const action = spawn(mainScript);
-action.stdout.on("data", (data) => {
-	console.log(`stdout: ${data}`);
-});
-
-action.on("close", (code) => {
-	console.log(`child process close all stdio with code ${code}`);
-});
-
-action.on("exit", (code) => {
-	console.log(`child process exited with code ${code}`);
-});
+const mainScript = `${__dirname}/${binary}`;
+const action = spawnSync(mainScript, []);
+console.log(result.stdout.toString());
